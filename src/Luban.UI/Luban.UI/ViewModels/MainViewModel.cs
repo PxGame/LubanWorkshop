bdfb2d105b46303aa4100967c81abf8a86ffa849
PeterXiang <565067150@qq.com>
2025-07-26 02:45:05 +0800

    添加点击计数功能和更新用户界面
    在 `MainViewModel.cs` 中新增整型属性 `_value`，并实现 `OnClick` 命令方法以更新点击次数和问候语。在 `MainView.axaml` 中优化设计时数据上下文，使用 `StackPanel` 包裹 `TextBlock` 和新增的按钮，增强用户交互体验。

--- src/Luban.UI/Luban.UI/ViewModels/MainViewModel.cs
+++ src/Luban.UI/Luban.UI/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Luban.UI.ViewModels;
 
@@ -6,4 +7,14 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _greeting = "Welcome to Avalonia!";
+
+    [ObservableProperty]
+    private int _value = 0;
+
+    [RelayCommand]
+    private void OnClick()
+    {
+        Value++;
+        Greeting = $"You clicked {Value} times!";
+    }
 }
