96c4746b0fd8d4e772bd188e113adf3fff22fc70
PeterXiang <565067150@qq.com>
2025-07-27 19:27:02 +0800

    增加计数功能和异步读取功能
    在 `MainViewModel.cs` 中添加了 `_count` 字段及 `OnAddCountClick` 和 `OnReadRemote` 命令方法，支持计数和异步操作。引入了必要的命名空间以支持命令和异步功能。
    
    在 `MainView.axaml` 中更新了设计时数据上下文，使用 `StackPanel` 替换原有的 `TextBlock`，并添加了两个按钮以增强用户交互体验。

--- src/Luban.UI/Luban.UI/ViewModels/MainViewModel.cs
+++ src/Luban.UI/Luban.UI/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
 
 namespace Luban.UI.ViewModels;
 
@@ -6,4 +8,27 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _greeting = "Welcome to Avalonia!";
+
+    private int _count;
+
+    [RelayCommand]
+    private void OnAddCountClick()
+    {
+        _count++;
+        Greeting = $"You clicked {_count} times.";
+    }
+
+    [RelayCommand]
+    private async Task OnReadRemote()
+    {
+        Greeting = "start ...";
+        for (int i = 0; i < 4; i++)
+        {
+            await Task.Delay(1000);
+
+            Greeting = $"wait {i}s ...";
+        }
+
+        Greeting = "completed !";
+    }
 }
